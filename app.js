3de3a01f4f8dd12371f9e2dcfc4af5bf1c34ebb2
kailashTuta <kailashTuta@users.noreply.github.com>
2020-10-03 08:13:59 +0530

    Added Api

--- app.js
+++ app.js
@@ -16,12 +16,26 @@ weatherApp.config(function ($routeProvider) {
         });
 });
 
+//SERVICES
+weatherApp.service('cityService', function () {
+    this.city = "New York, NY";
+});
+
 // CONTROLLER
 
-weatherApp.controller('homeController', ['$scope', function ($scope) {
+weatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, cityService) {
+    $scope.city = cityService.city;
 
+    $scope.$watch('city', function () {
+        cityService.city = $scope.city;
+    });
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function ($scope) {
+weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function ($scope, $resource, cityService) {
+    $scope.city = cityService.city;
+    $scope.weatherAPI = $resource("http://api.weatherapi.com/v1/forecast.json?key=a19e5510d499452094b24114200310", { callback: "JSON_CALLBACK" }, + '&'+ { get: { method: "JSONP" }});
+    // console.log($scope.weatherAPI);
 
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city });
+    console.log($scope.weatherResult);
 }]);
